ec1efe614db9edfcc9ea1090dbfb9eb031fe3a5d
shalabh147 <shalabh147@gmail.com>
2019-05-22 13:59:25 +0530

    Added classes

---
+++ camera.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+class camera{
+
+	vector<float> lookat(3);
+	vector<float> eye(3);
+	vector<float> up(3);
+	double fov;
+	double far;
+	double near;
+
+public:
+	
+
+
+
+};
