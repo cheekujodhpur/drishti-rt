18b5a2030c36e9e98a87d812daf4fbcbc094ffa3
shalabh147 <shalabh147@gmail.com>
2019-05-28 18:10:43 +0530

    Added parser for material

--- parser.cpp
+++ parser.cpp
@@ -77,9 +77,83 @@ camera nodeToCamera(TiXmlElement* Camera)
     return result;
 }
 
+image nodeToImage(TiXmlElement* Image)
+{
+	image result;
+	TiXmlElement* element = Image->FirstChildElement();
+	if(element && strcmp(element->Value(),"width")==0)
+		result.setWidth(doubleVal(element,"width"));
+	element=element->NextSiblingElement();
+
+	if(element && strcmp(element->Value(),"height")==0)
+		result.setHeight(doubleVal(element,"height"));
+	element=element->NextSiblingElement();
+
+	if(element && strcmp(element->Value(),"bgcolor")==0)
+		result.setBgcolor(nodeTovector(element));
+	element=element->NextSiblingElement();
+
+	return result;
+
+}
+
+image nodeToMaterials(TiXmlElement* Material)
+{
+	material result;
+	result=new Simplemat();
+	TiXmlElement* element= Material->FirstChildElement();
+
+	result.setId(doubleVal(Material,"simplemat"));
+
+	if(element && strcmp(element->Value(),"diffuse")==0)
+		result.setDiffuse(nodeTovector(element));
+	element=element->NextSiblingElement();
+
+	if(element && strcmp(element->Value(),"specular")==0)
+		result.setSpecular(nodeTovector(element));
+	element=element->NextSiblingElement();
+
+	if(element && strcmp(element->Value(),"reflect")==0)
+		result.setReflect(nodeTovector(element));
+	element=element->NextSiblingElement();
+
+	if(element && strcmp(element->Value(),"transmit")==0)
+		result.setTransmit(nodeTovector(element));
+	element=element->NextSiblingElement();
+
+	if(element && strcmp(element->Value(),"eta")==0)
+		result.setEta(doubleVal(element,"eta"));
+	element=element->NextSiblingElement();
+
+	if(element && strcmp(element->Value(),"n")==0)
+		result.setN(doubleVal(element,"n"));
+	element=element->NextSiblingElement();
+
+	if(element && strcmp(element->Value(),"isreflect")==0)
+		result.setIsreflect(doubleVal(element,"isreflect"));
+	element=element->NextSiblingElement();
+
+	if(element && strcmp(element->Value(),"istransmit")==0)
+		result.setIstransmit(doubleVal(element,"istransmit"));
+	element=element->NextSiblingElement();
+
+	return result;
+
+
+
+
+}
+
+
 int main(){
     TiXmlDocument doc("./scenes/sample-scene.xml");
     
+    vector<camera> cameralist;
+    vector<image> imagelist;
+    vector<materials> materialslist;
+    vector<objects> objectlist;
+    vector<lights> lightslist;
+
     if(!doc.LoadFile())
         throw runtime_error("bad parse");
 
@@ -89,16 +163,33 @@ int main(){
         
     for(TiXmlElement* a=root->FirstChildElement();a;a=a->NextSiblingElement()){
         if(strcmp(a->Value(),"camera")==0)
-            objectlist.push_back(nodeToCamera(a));
-        /*else if(strcmp(a->Value(),"image")==0)
-            objectlist.push_back(nodeToImage(a));
+            cameralist.push_back(nodeToCamera(a));
+        else if(strcmp(a->Value(),"image")==0)
+            imagelist.push_back(nodeToImage(a));
         else if(strcmp(a->Value(),"materials")==0)
-            objectlist.push_back(nodeToMaterials(a));
+        {
+        	for(TiXmlElement* b=a->FirstChildElement();b;b->NextSiblingElement())
+        	{
+        		materialslist.push_back(nodeTomaterials(b));
+        	}
+           
+        }
         else if(strcmp(a->Value(),"objects")==0)
-            objectlist.push_back(nodeToObjects(a));
+        {    
+        	for(TiXmlElement* b=a->FirstChildElement();b;b->NextSiblingElement())
+        	{
+        		objectslist.push_back(nodeToobjects(b));
+        	}
+        }	
+
         else if(strcmp(a->Value(),"lights")==0)
-            objectlist.push_back(nodeToLights(a));
-        else if(strcmp(a->Value(),"integrator")==0)//need to edit this
-            objectlist.push_back(nodeToIntegrator(a));*/
+        {
+           for(TiXmlElement* b=a->FirstChildElement();b;b->NextSiblingElement())
+        	{
+        		lightslist.push_back(nodeTolights(b));
+        	}
+        }	
+        /*else if(strcmp(a->Value(),"integrator")==0)//need to edit this
+            integratorlist.push_back(nodeToIntegrator(a));*/
         }
 }
