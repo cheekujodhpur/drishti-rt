36ce6d2f172d1a9e0a44b364a0d7d547a6296e6d
shalabh147 <shalabh147@gmail.com>
2019-05-28 10:20:06 +0530

    ADDED PARSER FILES

---
+++ parser.cpp
@@ -0,0 +1,71 @@
+#include <exception>
+#include  // cout
+#include <stdexcept> // runtime_error
+#include <cstdlib> // EXIT_FAILURE
+#include  // strcmp
+#include <vector>
+#include <tinyxml.h>
+#include "camera.hpp"
+#include "image.hpp"
+#include "materials.hpp"
+#include "objects.hpp"
+#include "lights.hpp"
+#include "integrator.hpp"
+
+using namespace std;
+
+const char* textValue(TiXmlElement* e)
+{
+    TiXmlNode* first=e->FirstChild();
+    if(first!=0 && first==e->LastChild() && first->Type()==TiXmlNode::TEXT)
+    {
+        return first->Value();
+    }
+     else {
+throw runtime_error(string("bad ") + e->Value( ) + " element");
+}
+}
+
+vector<double> nodeTovector(TiXmlElement* element)
+{
+    //unable to find out how to allocate those values in the string to values of vector.
+}
+camera nodeToCamera(TiXmlElement* Camera)
+{
+    camera result;
+    TiXmlElement* element = Camera->FirstChildElement();
+    if(element && strcmp(element->Value(),"lookat")==0)
+    {
+        result.setLookat(nodeTovector(element));
+    }
+    element=element->NextSiblingElement();
+    if(element && strcmp(element->Value(),"eye")==0)
+    {
+        result.setEye(nodeTovector(element));
+    }
+    element=element->NextSiblingElement();
+    if(element && strcmp(element->Value(),"up")==0)
+    {
+        result.setUp(nodeTovector(element));
+    }
+   element=element->NextSiblingElement();
+     if(element && strcmp(element->Value(),"fov")==0)
+    {
+        result.setFov(element->Attribute("angle"));
+    }
+     element=element->NextSiblingElement();
+    if(element && strcmp(element->Value(),"near")==0)
+    {
+        result.setNear(element->Attribute("double"));
+    }
+    element=element->NextSiblingElement();
+    if(element && strcmp(element->Value(),"far")==0)
+    {
+        result.setFar(element->Attribute("double"));
+    }
+
+
+}
+
+//setFar,setNear etc are some of the functions we need to make in camera class.
+}
